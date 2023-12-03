dba164af9554913b808d30a3717b115441ece0bf
Alsu.Shareeva@evraz.com <Shareeva_AM@emi.evraz.com>
2023-12-03 23:15:27 +0300

    -

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/gradient_ui/',
 	css: {
 		modules: {
 			generateScopedName: '[folder]_[local]__[hash:base64:5]',
